3c489b723af9eaa4941dc2dfdf5cdbce53b0c57d
Tayyab Naeem <tayyabnaeem187@gmail.com>
2023-08-29 18:30:47 +0500

    update. code

--- lib/data/hive_database.dart
+++ lib/data/hive_database.dart
@@ -1,5 +1,9 @@
 class HiveDatabase {
   /// refrece our hive
-  ///
+
   /// check weather data is already stored if not ,record the start data
+  /// return start date as yyyy/mm
+  /// write data
+  ///read data and return a List of workout
+  /// check if any exercise have been done
 }
